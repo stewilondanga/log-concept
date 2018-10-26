7039e54cdcdff3624380d341ed9985a3b9acb26e
Stewart ilondanga <stilondanga@gmail.com>
2018-10-26 11:31:02 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -30,29 +30,29 @@ $(document).ready(function() {
         elem.append($ripple);
       };
 
-      /*          $(document).on("click", ".login__submit", function(e) {
-                  if (animating) return;
-                  animating = true;
-                  var that = this;
-                  ripple($(that), e);
-                  $(that).addClass("processing");
-                  setTimeout(function() {
-                    $(that).addClass("success");
-                    setTimeout(function() {
-                      $app.show();
-                      $app.css("top");
-                      $app.addClass("active");
-                    }, submitPhase2 - 70);
-                    setTimeout(function() {
-                      $login.hide();
-                      $login.addClass("inactive");
-                      animating = false;
-                      $(that).removeClass("success processing");
-                    }, submitPhase2);
-                  }, submitPhase1);
-                });
+      $(document).on("click", ".login__submit", function(e) {
+        if (animating) return;
+        animating = true;
+        var that = this;
+        ripple($(that), e);
+        $(that).addClass("processing");
+        setTimeout(function() {
+          $(that).addClass("success");
+          setTimeout(function() {
+            $app.show();
+            $app.css("top");
+            $app.addClass("active");
+          }, submitPhase2 - 70);
+          setTimeout(function() {
+            $login.hide();
+            $login.addClass("inactive");
+            animating = false;
+            $(that).removeClass("success processing");
+          }, submitPhase2);
+        }, submitPhase1);
+      });
 
-                $(document).on("click", ".app__logout", function(e) {
+      /*          $(document).on("click", ".app__logout", function(e) {
                   if (animating) return;
                   $(".ripple").remove();
                   animating = true;
